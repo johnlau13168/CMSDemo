1d27bc555305dade7380b6c2fb62a36ab5081c74
johnlau13168 <johnlau13168@yahoo.com>
2017-06-27 13:22:58 +0800

    Add files via upload

---
+++ RequestTypeVM.js
@@ -0,0 +1,28 @@
+﻿function RequestTypeViewModel() {
+    var self = this;
+  //  alert("getrequesttypelist call");
+    self.requesttype = ko.observableArray([]);
+    
+   // alert(">>>>>>>>>>>>  context >>>>>" + "${pageContext.request.contextPath}");
+         var urlx = "/itsmportal/itsm//srvrqs/servicerequest/getrequesttypelist";
+           $.ajax({
+						type: "GET",
+						url: urlx,
+						contentType: "application/json",
+				        dataType: "json",
+						cache: false,	
+						async: false,
+	    	success: function(JSONdataFromServer) {
+        		     var mappedData = ko.utils.arrayMap(JSONdataFromServer, function(eachrequesttype) {
+                         
+        		    	 self.requesttype.push(eachrequesttype);
+        		    	 
+		    	      });
+		              }
+		        });
+};
+requesttypevm = new RequestTypeViewModel();
+ko.applyBindings(requesttypevm, document.getElementById('templatelist'));
+
+
+
